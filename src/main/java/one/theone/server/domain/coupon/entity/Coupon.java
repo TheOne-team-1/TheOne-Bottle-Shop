f0fe284f59bb305be21be27916f86ce23c015166
곽현민 <hunmin111@gmail.com>
2026-03-09 14:18:25 +0900

    feat: 쿠폰 엔티티 추가

---
+++ src/main/java/one/theone/server/domain/coupon/entity/Coupon.java
@@ -0,0 +1,45 @@
+package one.theone.server.domain.coupon.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import one.theone.server.common.entity.BaseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "coupons")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Coupon extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CouponUseType useType;
+
+    @Column(nullable = false)
+    private Long minPrice;
+
+    @Column(nullable = false)
+    private Long discountValue;
+
+    @Column(nullable = false)
+    private Long availQuantity;
+
+    @Column(nullable = false)
+    private LocalDateTime startAt;
+
+    private LocalDateTime endAt;
+
+    public enum CouponUseType {
+        AMOUNT, RATE
+    }
+}
