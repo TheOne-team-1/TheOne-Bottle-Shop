50c29e05f3752d3b3dbaa42ee07bad0a8beb9d93
곽현민 <hunmin111@gmail.com>
2026-03-09 19:10:39 +0900

    feat: 이벤트 보상 엔티티의 생성자 추가

--- src/main/java/one/theone/server/domain/event/entity/EventReward.java
+++ src/main/java/one/theone/server/domain/event/entity/EventReward.java
@@ -27,6 +27,22 @@ public class EventReward extends BaseEntity {
 
     private Long freebieId;
 
+    public static EventReward couponReward(Long eventId, Long couponId) {
+        EventReward reward = new EventReward();
+        reward.eventId = eventId;
+        reward.rewardType = EventRewardType.COUPON;
+        reward.couponId = couponId;
+        return reward;
+    }
+
+    public static EventReward freebieReward(Long eventId, Long freebieId) {
+        EventReward reward = new EventReward();
+        reward.eventId = eventId;
+        reward.rewardType = EventRewardType.FREEBIE;
+        reward.freebieId = freebieId;
+        return reward;
+    }
+
     public enum EventRewardType {
         COUPON, FREEBIE
     }
