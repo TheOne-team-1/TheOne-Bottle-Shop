f8b0f57783b8e7bd3f1482ffc4dfc267afc34bc5
Soomin <soomin02094@gmail.com>
2026-03-11 13:59:32 +0900

    refactor: 카테고리 예외 enum 분리

--- src/main/java/one/theone/server/common/exception/domain/CategoryExceptionEnum.java
+++ src/main/java/one/theone/server/common/exception/domain/CategoryExceptionEnum.java
@@ -5,13 +5,13 @@ import one.theone.server.common.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public enum ProductExceptionEnum implements ErrorCode {
-    ERR_PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다");
+public enum CategoryExceptionEnum implements ErrorCode {
+    ERR_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리를 찾을 수 없습니다");
 
     private final HttpStatus httpStatus;
     private final String message;
 
-    ProductExceptionEnum(HttpStatus httpStatus, String message) {
+    CategoryExceptionEnum(HttpStatus httpStatus, String message) {
         this.httpStatus = httpStatus;
         this.message = message;
     }
