456722ca5055799e701871d247185ad813e2ff17
곽현민 <hunmin111@gmail.com>
2026-03-11 16:15:51 +0900

    feat: 로컬 캐시를 적용한 상품 검색의 서비스 구현

---
+++ src/main/java/one/theone/server/domain/search/service/SearchCacheService.java
@@ -0,0 +1,28 @@
+package one.theone.server.domain.search.service;
+
+import lombok.RequiredArgsConstructor;
+import one.theone.server.common.dto.PageResponse;
+import one.theone.server.domain.product.repository.ProductRepository;
+import one.theone.server.domain.search.dto.ProductSearchResponse;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static one.theone.server.common.config.cache.CacheConfig.PRODUCT_SEARCH;
+
+@Service
+@RequiredArgsConstructor
+public class SearchCacheService {
+
+    private final ProductRepository productRepository;
+
+    @Cacheable(
+            value = PRODUCT_SEARCH,
+            key = "'keyword:' + #keyword + ':page:' + #pageable.pageNumber + ':size:' + #pageable.pageSize",
+            cacheManager = "localCacheManager")
+    @Transactional(readOnly = true)
+    public PageResponse<ProductSearchResponse> getOrCache(String keyword, Pageable pageable) {
+        return productRepository.findProductByKeyword(keyword, pageable);
+    }
+}
