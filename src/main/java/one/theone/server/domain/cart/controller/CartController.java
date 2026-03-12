b1ec6dfa2a3940ed58c8ee25d3166cb9ed429a12
popo2381 <popo2381@icloud.com>
2026-03-12 14:51:43 +0900

    feat(cart): 장바구니 상품 추가 API 구현
    - CartController addItem API 추가
    - CartService addItem 호출 로직 연결
    - BaseResponse 기반 공통 응답 구조 적용
    - HTTP 201 CREATED 응답 반환

--- src/main/java/one/theone/server/domain/cart/controller/CartController.java
+++ src/main/java/one/theone/server/domain/cart/controller/CartController.java
@@ -1,11 +1,26 @@
 package one.theone.server.domain.cart.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.theone.server.common.dto.BaseResponse;
+import one.theone.server.domain.cart.dto.request.CartAddRequest;
+import one.theone.server.domain.cart.dto.response.CartAddResponse;
+import one.theone.server.domain.cart.service.CartService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/carts")
 public class CartController {
+    private final CartService cartService;
+
+    @PostMapping("/items")
+    public ResponseEntity<BaseResponse<CartAddResponse>> addItem(
+            @RequestParam Long memberId,
+            @RequestBody CartAddRequest request) {
+        CartAddResponse response = cartService.addItem(memberId, request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(BaseResponse.success(HttpStatus.CREATED.name(), "장바구니 상품 등록 성공", response));
+    }
 }
