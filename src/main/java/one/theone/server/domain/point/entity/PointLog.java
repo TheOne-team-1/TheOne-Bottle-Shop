4e01a0e8213761a88891d9de31391178847ebd78
Soomin <soomin02094@gmail.com>
2026-03-13 15:08:11 +0900

    feat: 포인트 관련 엔티티 추가

---
+++ src/main/java/one/theone/server/domain/point/entity/PointLog.java
@@ -0,0 +1,38 @@
+package one.theone.server.domain.point.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import one.theone.server.common.entity.BaseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "point_logs")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PointLog extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    private Long orderId;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PointType type;
+
+    private Long amount;
+
+    private Long balanceSnap;
+
+    private LocalDateTime expiredAt;
+
+    public enum PointType {
+        EARN, USE, EXPIRED, ADMIN
+    }
+}
