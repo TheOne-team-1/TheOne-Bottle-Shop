47696da18b260a879cad02d79f1bed2829ddeca6
곽현민 <hunmin111@gmail.com>
2026-03-16 12:16:03 +0900

    feat: 이벤트 목록 조회의 동적 쿼리 구현

--- src/main/java/one/theone/server/domain/event/repository/EventQueryRepositoryImpl.java
+++ src/main/java/one/theone/server/domain/event/repository/EventQueryRepositoryImpl.java
@@ -1,13 +1,19 @@
 package one.theone.server.domain.event.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import one.theone.server.common.dto.PageResponse;
 import one.theone.server.domain.event.dto.EventsGetResponse;
 import one.theone.server.domain.event.entity.Event;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
 import java.util.List;
+
+import static one.theone.server.domain.event.entity.QEvent.event;
 
 @RequiredArgsConstructor
 public class EventQueryRepositoryImpl implements EventQueryRepository{
@@ -16,6 +22,33 @@ public class EventQueryRepositoryImpl implements EventQueryRepository{
 
     @Override
     public PageResponse<EventsGetResponse> findEventsWithConditions(Pageable pageable, List<Event.EventStatus> statuses) {
-        return null;
+        List<EventsGetResponse> content = queryFactory
+                .select(Projections.constructor(EventsGetResponse.class,
+                        event.id,
+                        event.name,
+                        event.type,
+                        event.status,
+                        event.startAt,
+                        event.endAt))
+                .from(event)
+                .where(
+                        event.deleted.isFalse(),
+                        event.status.in(statuses)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory
+                .select(event.count())
+                .from(event)
+                .where(
+                        event.deleted.isFalse(),
+                        event.status.in(statuses)
+                );
+
+        Page<EventsGetResponse> page = PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+
+        return PageResponse.register(page);
     }
 }
