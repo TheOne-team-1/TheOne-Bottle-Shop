6c3a51acbba03f754b2c66631b858e316f07000e
popo2381 <popo2381@icloud.com>
2026-03-16 17:50:52 +0900

    feat(order): 주문 조회 API 구현
    - 주문 목록 조회 API 추가
    - 주문 상세 조회 API 추가
    - 주문 조회 서비스 로직 연동

--- src/main/java/one/theone/server/domain/order/controller/OrderController.java
+++ src/main/java/one/theone/server/domain/order/controller/OrderController.java
@@ -5,13 +5,14 @@ import one.theone.server.common.dto.BaseResponse;
 import one.theone.server.domain.order.dto.request.OrderCreateDirectRequest;
 import one.theone.server.domain.order.dto.request.OrderCreateFromCartRequest;
 import one.theone.server.domain.order.dto.response.OrderCreateResponse;
+import one.theone.server.domain.order.dto.response.OrderDetailGetResponse;
+import one.theone.server.domain.order.dto.response.OrderListGetResponse;
 import one.theone.server.domain.order.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,5 +36,24 @@ public class OrderController {
         OrderCreateResponse response = orderService.createOrderFromCart(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(BaseResponse.success(HttpStatus.CREATED.name(), "장바구니 주문 생성 성공", response));
+    }
+
+    @GetMapping
+    public ResponseEntity<BaseResponse<List<OrderListGetResponse>>> getOrderList(
+            @RequestParam Long memberId
+    ) {
+        List<OrderListGetResponse> response = orderService.getOrderList(memberId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseResponse.success(HttpStatus.OK.name(), "주문 목록 조회 성공", response));
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<BaseResponse<OrderDetailGetResponse>> getOrderList(
+            @RequestParam Long memberId,
+            @PathVariable Long orderId
+    ) {
+        OrderDetailGetResponse response = orderService.getOrderDetail(memberId, orderId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseResponse.success(HttpStatus.OK.name(), "주문 상세 조회 성공", response));
     }
 }
