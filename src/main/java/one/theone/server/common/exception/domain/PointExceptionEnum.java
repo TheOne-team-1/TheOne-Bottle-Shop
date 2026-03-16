f3f08c040627735567566350e650bf83da647b6f
Soomin <soomin02094@gmail.com>
2026-03-16 15:20:55 +0900

    feat: 포인트 서비스 분산락 적용으로 동시성 제어

--- src/main/java/one/theone/server/common/exception/domain/PointExceptionEnum.java
+++ src/main/java/one/theone/server/common/exception/domain/PointExceptionEnum.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 public enum PointExceptionEnum implements ErrorCode {
     ERR_INSUFFICIENT_POINT(HttpStatus.BAD_REQUEST, "포인트 잔액이 부족합니다"),
     ERR_POINT_LOG_NOT_FOUND(HttpStatus.NOT_FOUND, "포인트 로그를 찾을 수 없습니다"),
-    ERR_POINT_ALREADY_REFUNDED(HttpStatus.BAD_REQUEST, "이미 환불된 포인트입니다");
+    ERR_POINT_ALREADY_REFUNDED(HttpStatus.BAD_REQUEST, "이미 환불된 포인트입니다"),
+    ERR_POINT_LOCK_FAILED(HttpStatus.CONFLICT, "잠시 후 다시 시도해주세요");
 
     private final HttpStatus httpStatus;
     private final String message;
