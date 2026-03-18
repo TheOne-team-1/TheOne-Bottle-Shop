c1d4fdf8ec4ebe4a599fe09faa0a127db269b1cd
KYJ <nananan1213@gmail.com>
2026-03-18 12:37:56 +0900

    feat: 구매 확정 스케줄러

--- src/main/java/one/theone/server/domain/payment/repository/PaymentRepository.java
+++ src/main/java/one/theone/server/domain/payment/repository/PaymentRepository.java
@@ -3,5 +3,8 @@ package one.theone.server.domain.payment.repository;
 import one.theone.server.domain.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByOrderId(Long orderId);
 }
